39e50053ecee94d0a87737e541bcd2d761f9a868
Simón Rodríguez <47600918+simonrodrigueza@users.noreply.github.com>
2020-05-19 09:25:34 -0500

    first commit

---
+++ src/java/Controlador/Controlador.java
@@ -0,0 +1,107 @@
+
+package Controlador;
+
+import Modelo.Persona;
+import ModeloDAO.PersonaDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Controlador extends HttpServlet {
+
+    String listar="vistas/listar.jsp";
+    String add="vistas/add.jsp";
+    String edit="vistas/edit.jsp";
+    Persona p=new Persona();
+    PersonaDAO dao=new PersonaDAO();
+    int id;
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Controlador</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Controlador at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String acceso="";
+        String action=request.getParameter("accion");
+        if(action.equalsIgnoreCase("listar")){
+            acceso=listar;            
+        }else if(action.equalsIgnoreCase("add")){
+            acceso=add;
+        }
+        else if(action.equalsIgnoreCase("Agregar")){
+            String cedula=request.getParameter("txtCedula");
+            String nombre=request.getParameter("txtNombre");
+            int edad=Integer.parseInt(request.getParameter("txtEdad"));
+            String cargo=request.getParameter("txtCargo");
+            int salario=Integer.parseInt(request.getParameter("txtSalario"));
+            p.setCedula(cedula);
+            p.setNombre(nombre);
+            p.setEdad(edad);
+            p.setCargo(cargo);
+            p.setSalario(salario);
+            dao.add(p);
+            acceso=listar;
+        }
+        else if(action.equalsIgnoreCase("editar")){
+            request.setAttribute("idper",request.getParameter("id"));
+            acceso=edit;
+        }
+        else if(action.equalsIgnoreCase("Actualizar")){
+            id=Integer.parseInt(request.getParameter("txtId"));
+            String cedula=request.getParameter("txtCedula");
+            String nombre=request.getParameter("txtNombre");
+            int edad=Integer.parseInt(request.getParameter("txtEdad"));
+            String cargo=request.getParameter("txtCargo");
+            int salario=Integer.parseInt(request.getParameter("txtSalario"));
+            p.setId(id);
+            p.setCedula(cedula);
+            p.setNombre(nombre);
+            p.setEdad(edad);
+            p.setCargo(cargo);
+            p.setSalario(salario);
+            dao.edit(p);
+            acceso=listar;
+        }
+        else if(action.equalsIgnoreCase("eliminar")){
+            id=Integer.parseInt(request.getParameter("id"));
+            p.setId(id);
+            dao.eliminar(id);
+            acceso=listar;
+        }
+        RequestDispatcher vista=request.getRequestDispatcher(acceso);
+        vista.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+   
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
